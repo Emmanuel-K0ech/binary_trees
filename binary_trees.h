24c9389d6848096ad4098611f21021712510a462
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-13 13:46:23 -0700

    Updating header file

--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,9 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Library headers */
+#include <stddef.h>
+
 /* Functions */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
