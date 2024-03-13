d16e0f42172524e13be5b855fa56e551233d0d1c
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-03-13 03:05:51 -0700

    Task 2: Logical Error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,11 +17,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = NULL;
 	new_node->parent = parent;
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
+	parent->right = new_node;
 	return (new_node);
 }
